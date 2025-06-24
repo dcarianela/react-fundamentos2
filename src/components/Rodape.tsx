e8752e5e58ba68e26a9583b4e337d2cfe9208306
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-24 09:46:06 -0300

    cria classe para rodape

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,8 +1,15 @@
 export default function Rodape() {
   return (
-    <footer>
-      <h2>Pequeno site criado com React</h2>
-      <p>desenvolvido por eu</p>
-    </footer>
+    <>
+      <footer>
+        <h2>Pequeno site criado com React</h2>
+        <p>
+          desenvolvido por eu <b>Hayakawa</b> &copy; 2025{" "}
+        </p>
+      </footer>
+      <hr />
+      <p className="outro-texto">Qualquer coisa...</p>
+      <hr />
+    </>
   );
 }
