ed73ab6e855e220d351144736e8ec998c2433fe3
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-24 09:20:30 -0300

    aaaaaa

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,20 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempore
+          incidunt aliquid reprehenderit nemo id dolorem nulla, sint debitis
+          eveniet doloribus culpa expedita ipsum, facilis beatae eligendi
+          quisquam odit consequuntur.
+        </span>
+        <span>
+          Atque voluptatibus eum illum eligendi tempora nam accusantium, dolorum
+          et animi. Iste voluptatem quas aut eum excepturi magni incidunt neque
+          autem perspiciatis similique? Sequi quos eum quaerat sed dolor rem!
+        </span>
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,59 +7,9 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos de React</h2>
         <p>Este é um exemplo da aplicação React.</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo</h3>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-              tempore incidunt aliquid reprehenderit nemo id dolorem nulla, sint
-              debitis eveniet doloribus culpa expedita ipsum, facilis beatae
-              eligendi quisquam odit consequuntur.
-            </span>
-            <span>
-              Atque voluptatibus eum illum eligendi tempora nam accusantium,
-              dolorum et animi. Iste voluptatem quas aut eum excepturi magni
-              incidunt neque autem perspiciatis similique? Sequi quos eum
-              quaerat sed dolor rem!
-            </span>
-          </p>
-        </article>
-
-        <article>
-          <h3>Artigo 2 de exemplo</h3>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-              tempore incidunt aliquid reprehenderit nemo id dolorem nulla, sint
-              debitis eveniet doloribus culpa expedita ipsum, facilis beatae
-              eligendi quisquam odit consequuntur.
-            </span>
-            <span>
-              Atque voluptatibus eum illum eligendi tempora nam accusantium,
-              dolorum et animi. Iste voluptatem quas aut eum excepturi magni
-              incidunt neque autem perspiciatis similique? Sequi quos eum
-              quaerat sed dolor rem!
-            </span>
-          </p>
-        </article>
-
-        <article>
-          <h3>Artigo 3 de exemplo</h3>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-              tempore incidunt aliquid reprehenderit nemo id dolorem nulla, sint
-              debitis eveniet doloribus culpa expedita ipsum, facilis beatae
-              eligendi quisquam odit consequuntur.
-            </span>
-            <span>
-              Atque voluptatibus eum illum eligendi tempora nam accusantium,
-              dolorum et animi. Iste voluptatem quas aut eum excepturi magni
-              incidunt neque autem perspiciatis similique? Sequi quos eum
-              quaerat sed dolor rem!
-            </span>
-          </p>
-        </article>
+        <Artigo />
+        <Artigo />
+        <Artigo />
       </section>
     </main>
   );
