42db6f7d457469422c91da124f312d3a258124e3
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-26 09:10:52 -0300

    configura prop opcional e valor padrão

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -13,7 +13,7 @@ export default function Conteudo() {
       >
         <h2 className="font-bold text-lg">Fundamentos de React!</h2>
 
-        <Saudacao nome="Hayakawa" classe="bg-emerald-100" />
+        <Saudacao classe="bg-emerald-100" />
         <Saudacao nome="Kobeni" classe="bg-red-100" />
         <p>Este é um exemplo da aplicação React.</p>
 
--- src/components/Conteudo/Saudacao.tsx
+++ src/components/Conteudo/Saudacao.tsx
@@ -1,10 +1,14 @@
 /* Declara um tipo que será usado pelas props do Componente */
 type Props = {
-  nome: string;
+  nome?: string; // prop opcional
   classe: string;
 };
 
-export default function Saudacao({ nome, classe }: Props) {
+export default function Saudacao({
+  // Caso nome não seja informado (opcional), Visitante será usado
+  nome = "Visitante",
+  classe,
+}: Props) {
   return (
     <p className={`my-2 rounded text-center ${classe}`}>
       Bem-vindo(a) <b>{nome}</b>
