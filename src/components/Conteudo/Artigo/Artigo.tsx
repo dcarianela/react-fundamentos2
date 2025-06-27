91014183f3dfc7e4ffce108783d577e627c8b75e
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-27 11:08:20 -0300

    realiza atividade

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,15 +1,25 @@
 import estilos from "./Artigo.module.css";
 import type { Curso } from "../../../types/Curso";
+import { useState } from "react";
 
 type ArtigoProps = {
   dados: Curso;
 };
 
 export default function Artigo({ dados }: ArtigoProps) {
-  // Podemos receber a prop dados para depois desestruturar
+  const [selecionado, setSelecionado] = useState(false);
+
+  const alternarCor = () => {
+    setSelecionado(!selecionado);
+  };
+
   const { titulo, preco, categoria } = dados;
   return (
-    <article className={estilos.artigo}>
+    <article
+      className={estilos.artigo}
+      onClick={alternarCor}
+      style={{ backgroundColor: selecionado ? "yellow" : "#f9f9f9" }}
+    >
       <h3>{titulo}</h3>
       <p>
         <b>Categoria: </b>
