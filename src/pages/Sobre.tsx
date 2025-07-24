29055b7ff7f019acc4d7726969e950fe2829e07f
dcarianela <aki.hayakawa5577@gmail.com>
2025-07-24 09:53:29 -0300

    aaaaaa

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,16 @@
+export default function Sobre() {
+  return (
+    <section>
+      <h2>Sobre nossa empresa</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea nisi
+        illum. Rem ipsam commodi repellendus tempore aspernatur numquam
+        assumenda animi? Laudantium saepe maxime quo autem vel! Tempore,
+        obcaecati quo. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Alias et, deserunt dignissimos quasi cupiditate nemo, excepturi iste,
+        nam ab quisquam in at dolores. Hic suscipit numquam, culpa recusandae
+        rerum maiores?
+      </p>
+    </section>
+  );
+}
